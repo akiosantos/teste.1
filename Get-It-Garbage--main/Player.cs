3ce245fb28a557ae40afc3eb386490e6c6c250c8
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-06-13 08:51:40 -0300

    Add files via upload

---
+++ Get-It-Garbage--main/Player.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public  float   Speed;
+    public  float   JumpForce;
+   
+    public  bool    isJumping;
+    public  bool    doubleJump;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+     void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f,0f);
+        transform.position += movement * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walkl", false);
+            anim.SetBool("walkr", true);
+        }
+        
+         if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walkl", true);
+            anim.SetBool("walkr", false);
+        }
+
+        if(Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walkl", false);
+            anim.SetBool("walkr", false);
+
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                anim.SetBool("jump", true);
+
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+                
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+            anim.SetBool("jump", false);
+        }
+
+        if(collision.gameObject.tag == "Spike")
+        {
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
+
+        if(collision.gameObject.tag == "Saw")
+        {
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
+
+       
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = true;
+        }
+    }
+}
